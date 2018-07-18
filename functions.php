431f1d25eae1a65af0772184381d6063cfeb1245
MS <metasanjaya@gmail.com>
2018-07-18 17:26:45 +0700

    edit functions.php add action for fb comment

--- functions.php
+++ functions.php
@@ -6,6 +6,24 @@ function ms_theme_enqueue_styles() {
 add_action( 'wp_enqueue_scripts', 'ms_theme_enqueue_styles' );
 
 function ms_theme_wp_head() {
-    
+    ?>
+<script>
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+</script>
+
+    <?php
 }
 add_action( 'wp_head' , 'ms_theme_wp_head' );
+
+function ms_theme_wp_footer() {
+    ?>
+    <div id="fb-root"></div>
+    <?php
+}
+add_action( 'wp_footer', 'ms_theme_wp_footer' );

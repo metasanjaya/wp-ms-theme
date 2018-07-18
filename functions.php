09b65f4724bfaa0dfe94e956331e3a1885fcead0
MS <metasanjaya@gmail.com>
2018-07-18 16:34:52 +0700

    edit functions.php

--- functions.php
+++ functions.php
@@ -1,7 +1,11 @@
 <?php
 
-
-function my_theme_enqueue_styles() {
+function ms_theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'ms_theme_enqueue_styles' );
+
+function ms_theme_wp_head() {
+    
+}
+add_action( 'wp_head' , 'ms_theme_wp_head' );

6f2ef6b62e66f45cf5c9070cce4584ec05038481
MS <metasanjaya@gmail.com>
2018-07-19 11:19:20 +0700

    add if comments open

--- comments.php
+++ comments.php
@@ -1,4 +1,7 @@
-
+<?php 
+if (comments_open()) :
+?>
 <section id="comment-wrap">
   <div id="respond" class="fb-comments" data-href="<?php the_permalink(); ?>" data-numposts="10" data-width="100%"></div>
 </section>
+<?php endif; ?>
